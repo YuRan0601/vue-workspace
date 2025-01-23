f35e99a627104606ba8e34dc1edcceefcc8264c4
YuRan0601 <tp6m4b06to@gmail.com>
2025-01-23 21:33:13 +0800

    路由添加註解說明

--- cloud-serenity-hotel/src/router/index.js
+++ cloud-serenity-hotel/src/router/index.js
@@ -34,241 +34,280 @@ import MemberAccount from "@/views/user/MemberAccount.vue";
 import AttractionComponent from "@/components/Attraction/AttractionComponent.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        // {
-        //   path: '/',
-        //   name: 'home',
-        //   component: HomeView,
-        // }
+  history: createWebHistory(),
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // }
+
+    //最外層：登入頁
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+
+    //最外層：前台頁
+    {
+      path: "/front",
+      name: "front",
+      component: FrontView,
+      children: [
+
+        //---訂房前台---
         {
-            path: '/login',
-            name: 'login',
-            component: Login
-        },
-        {
-            path: '/front',
-            name: 'front',
-            component: FrontView,
-            children: [
-                {
-                    path: '/front/bookingHome',
-                    name: 'bookingHome',
-                    component: BookingHome,
-                    children: [
-                        {
-                            path: '/bookingSearch',
-                            name: 'bookingSearch',
-                            component: BookingSearchView,
-                        }
-                    ]
-                },
-                {
-                    path: '/front/booking/roomType',
-                    name: 'roomFront',
-                    component: RoomFront,
-                },
-                {
-                    path: '/front/member',
-                    name: 'memberFront',
-                    component: FrontMember,
-                    beforeEnter: async (to, from, next) => {
-
-
-                        const useStores = useAuthStore()
-                        useStores.fetchUser();
-                        // 檢查登入狀態
-                        if (useStores.user == null) {
-                            try {
-                                await useStores.fetchUser(); // 從後端取得使用者資料
-                                next();
-                            } catch (error) {
-                                console.log(error);
-                                next('/login'); // 無法取得資料 屬於未登入 則引導到登入頁面
-                            }
-                        } else { //已登入 則判斷身分 限制存取的頁面
-                            if (useStores.user.userIdentity == "user") {
-                                next();
-                            } else {
-                                alert('管理員不能去會員中心!')
-                                // useStores.logout();
-                                next('/back');
-                            }
-                        }
-                    },
-                    children: [
-                        {
-                            path: '/front/member/Overview',
-                            name: 'memberOverview',
-                            component: MemberOverview
-                        },
-                        {
-                            path: '/front/member/Info',
-                            name: 'memberInfo',
-                            component: MemberInfo
-                        },
-                        {
-                            path: '/front/member/Account',
-                            name: 'memberAccount',
-                            component: MemberAccount
-                        },
-
-                    ]
-                },
-                {
-                    path: '/front/register',
-                    name: 'register',
-                    component: Register
-                },
-
-                {
-                    path: 'Attraction/AttractionType',
-                    name: 'attractionFront',
-                    component: AttractionComponent,
-                },
-            ]
-        },
-        {
-            path: '/back',
-            name: 'back',
-            component: BackView,
-            beforeEnter: async (to, from, next) => {
-
-                const useStores = useAuthStore()
-
-                useStores.fetchUser();
-                console.log(useStores.user);
-
-                // 檢查登入狀態
-                if (useStores.user == null) {
-                    try {
-                        await useStores.fetchUser(); // 從後端取得使用者資料
-                        next();
-
-                    } catch (error) {
-                        console.log(error);
-                        next('/login'); // 無法取得資料 屬於未登入 則引導到登入頁面
-                    }
-                } else { //已登入 則判斷身分 限制存取的頁面
-                    if (useStores.user.userIdentity == "admin") {
-                        next();
-                    } else {
-                        alert('你不是管理員!')
-                        // useStores.logout();
-                        next('/front');
-                    }
-                }
+          path: "/front/bookingHome",
+          name: "bookingHome",
+          component: BookingHome,
+          children: [
+            {
+              path: "/bookingSearch",
+              name: "bookingSearch",
+              component: BookingSearchView,
             },
-            children: [
-                {
-                    path: '/bookingBack',
-                    name: 'bookingBack',
-                    component: BookingBack,
-                    children: [
-                        {
-                            path: '/back/booking/roomType',
-                            name: 'roomType',
-                            component: RoomTypeView
-                        },
-                        {
-                            path: '/back/booking/room',
-                            name: 'room',
-                            component: RoomView
-                        },
-                    ]
-                },
-                {
-                    path: '/productBack',
-                    name: 'productBack',
-                    component: ProductBack,
-                    children: [
-                        {
-                            path: '/ProductAll',
-                            name: 'ProductAll',
-                            component: ProductHome
-                        }
-                    ]
-                },
-                {
-                    path: '/memberBack',
-                    name: 'memberBack',
-                    component: BackMember
-                },
-                {
-                    path: '/adminBack',
-                    name: 'adminBack',
-                    component: BackAdmin
-                },
-                {
-                    path: '/rent/carHome', name: 'carHome', component: CarHome,
-                    children: [
-                        { path: '/rent/carType', name: 'carType', component: CarTypeView }
-                    ]
-                },
-                {
-                    path: '/rent/modelOperate/:id', name: 'modelOperate', component: ModelOperate, props: true,
-                    children: [
-                        { path: '/rent/carInfo/:id', name: 'carInfo', component: CarInfo, props: true },
-                        { path: 'carImage/:id', name: 'carImage', component: CarImage, props: true }
-                    ]
-                },
-                {
-                    path: '/rent/VehicleDetails', name: 'vehicleDetails', component: VehicleDetails,
-                    children: [
-                        { path: '/rent/VehicleList', name: 'vehicleList', component: VehicleList }
-                    ]
-                },
-                {
-                    path: '/orderBack',
-                    name: 'orderBack',
-                    component: OrderBack, // 訂單後台的主組件
-                    redirect: '/order/orderList', // 自動跳轉到 OrderListView
-                    children: [
-                      {
-                        path: '/order/orderList',
-                        name: 'orderList',
-                        component: OrderListView
-                      },
-                      {
-                        path: '/order/orderdetail',
-                        name: 'orderdetail',
-                        component: OrderDetailView,
-                        props: (route) => ({ orderId: route.query.orderId }), // 將 query 作為 props 傳入
-                      },
-                      {
-                        path: '/order/orderedit',
-                        name: 'orderedit',
-                        component: OrderEditView,
-                        props: (route) => ({ orderId: route.query.orderId }), // 將 query 作為 props 傳入
-                      },
-                      {
-                        path: '/order/orderadd',
-                        name: 'orderadd',
-                        component: OrderAddView,
-                      }
-                    ]
-                  }
-            ]
+          ],
         },
-<<<<<<< HEAD
-    ]
-})
-=======
         {
-          path: '/productBack',
-          name: 'productBack',
+          path: "/front/booking/roomType",
+          name: "roomFront",
+          component: RoomFront,
+        },
+        //-------------
+
+        //---會員前台---
+        {
+          path: "/front/member",
+          name: "memberFront",
+          component: FrontMember,
+          beforeEnter: async (to, from, next) => {
+            const useStores = useAuthStore();
+            useStores.fetchUser();
+            // 檢查登入狀態
+            if (useStores.user == null) {
+              try {
+                await useStores.fetchUser(); // 從後端取得使用者資料
+                next();
+              } catch (error) {
+                console.log(error);
+                next("/login"); // 無法取得資料 屬於未登入 則引導到登入頁面
+              }
+            } else {
+              //已登入 則判斷身分 限制存取的頁面
+              if (useStores.user.userIdentity == "user") {
+                next();
+              } else {
+                alert("管理員不能去會員中心!");
+                // useStores.logout();
+                next("/back");
+              }
+            }
+          },
+          children: [
+            {
+              path: "/front/member/Overview",
+              name: "memberOverview",
+              component: MemberOverview,
+            },
+            {
+              path: "/front/member/Info",
+              name: "memberInfo",
+              component: MemberInfo,
+            },
+            {
+              path: "/front/member/Account",
+              name: "memberAccount",
+              component: MemberAccount,
+            },
+          ],
+        },
+        {
+          path: "/front/register",
+          name: "register",
+          component: Register,
+        },
+        //-------------
+
+        //---景點前台---
+        {
+          path: "Attraction/AttractionType",
+          name: "attractionFront",
+          component: AttractionComponent,
+        },
+        //-------------
+
+      ],
+    },
+
+    //最外層：後台頁
+    {
+      path: "/back",
+      name: "back",
+      component: BackView,
+      beforeEnter: async (to, from, next) => {
+        const useStores = useAuthStore();
+
+        useStores.fetchUser();
+        console.log(useStores.user);
+
+        // 檢查登入狀態
+        if (useStores.user == null) {
+          try {
+            await useStores.fetchUser(); // 從後端取得使用者資料
+            next();
+          } catch (error) {
+            console.log(error);
+            next("/login"); // 無法取得資料 屬於未登入 則引導到登入頁面
+          }
+        } else {
+          //已登入 則判斷身分 限制存取的頁面
+          if (useStores.user.userIdentity == "admin") {
+            next();
+          } else {
+            alert("你不是管理員!");
+            // useStores.logout();
+            next("/front");
+          }
+        }
+      },
+      children: [
+
+        //---訂房後台---
+        {
+          path: "/bookingBack",
+          name: "bookingBack",
+          component: BookingBack,
+          children: [
+            {
+              path: "/back/booking/roomType",
+              name: "roomType",
+              component: RoomTypeView,
+            },
+            {
+              path: "/back/booking/room",
+              name: "room",
+              component: RoomView,
+            },
+          ],
+        },
+        //-------------
+
+
+        //---商品後台---
+        {
+          path: "/productBack",
+          name: "productBack",
           component: ProductBack,
           children: [
             {
-              path: '/productAll',
-              name: 'productAll',
-              component: ProductHome
-            }
-          ]
+              path: "/productAll",
+              name: "productAll",
+              component: ProductHome,
+            },
+          ],
         },
-        { path: "/productAdd", component: ProductAdd, name: "productAdd" },
-      ]
+        {
+          path: "/productAdd",
+          component: ProductAdd,
+          name: "productAdd",
+        },
+        //-------------
+
+        
+        //---會員後台---
+        {
+          path: "/memberBack",
+          name: "memberBack",
+          component: BackMember,
+        },
+        {
+          path: "/adminBack",
+          name: "adminBack",
+          component: BackAdmin,
+        },
+        //-------------
+
+
+        //---租車後台---
+        {
+          path: "/rent/carHome",
+          name: "carHome",
+          component: CarHome,
+          children: [
+            { path: "/rent/carType", name: "carType", component: CarTypeView },
+          ],
+        },
+        {
+          path: "/rent/modelOperate/:id",
+          name: "modelOperate",
+          component: ModelOperate,
+          props: true,
+          children: [
+            {
+              path: "/rent/carInfo/:id",
+              name: "carInfo",
+              component: CarInfo,
+              props: true,
+            },
+            {
+              path: "carImage/:id",
+              name: "carImage",
+              component: CarImage,
+              props: true,
+            },
+          ],
+        },
+        {
+          path: "/rent/VehicleDetails",
+          name: "vehicleDetails",
+          component: VehicleDetails,
+          children: [
+            {
+              path: "/rent/VehicleList",
+              name: "vehicleList",
+              component: VehicleList,
+            },
+          ],
+        },
+        //-------------
+
+        //---訂單後台---
+        {
+          path: "/orderBack",
+          name: "orderBack",
+          component: OrderBack, // 訂單後台的主組件
+          redirect: "/order/orderList", // 自動跳轉到 OrderListView
+          children: [
+            {
+              path: "/order/orderList",
+              name: "orderList",
+              component: OrderListView,
+            },
+            {
+              path: "/order/orderdetail",
+              name: "orderdetail",
+              component: OrderDetailView,
+              props: (route) => ({ orderId: route.query.orderId }), // 將 query 作為 props 傳入
+            },
+            {
+              path: "/order/orderedit",
+              name: "orderedit",
+              component: OrderEditView,
+              props: (route) => ({ orderId: route.query.orderId }), // 將 query 作為 props 傳入
+            },
+            {
+              path: "/order/orderadd",
+              name: "orderadd",
+              component: OrderAddView,
+            },
+          ],
+        },
+        //-------------
+
+      ],
     },
->>>>>>> product
+  ],
+});
 
 export default router;
