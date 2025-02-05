1fb66077c748716dae3b40707f4910bb840a6b94
xinoxo1120 <miaoxolin1120@gmail.com>
2025-02-05 12:52:34 +0800

    主頁圖並轉跳連結

--- cloud-serenity-hotel/src/views/common/index.vue
+++ cloud-serenity-hotel/src/views/common/index.vue
@@ -1,11 +1,11 @@
 <script setup>
-import BookingIndex from '@/components/booking/BookingIndex.vue';
-
-    
+import BookingIndex from "@/components/booking/BookingIndex.vue";
+import CarIndex from "@/components/rent/CarIndex.vue";
 </script>
 
 <template>
-    <div>
-        <BookingIndex></BookingIndex>
-    </div>
+  <div>
+    <BookingIndex></BookingIndex>
+    <CarIndex></CarIndex>
+  </div>
 </template>
